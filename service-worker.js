ffb24c89c012ff227f61350476d9428f94726ae2
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-12-22 11:47:09 +0100

    fix: removed useless retry on sw

--- service-worker.js
+++ service-worker.js
@@ -6,25 +6,3 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetchWithRetry(event.request, 10)
-  );
-});
-
-function fetchWithRetry(request, retries) {
-  return fetch(request).then(response => {
-    if (!response.ok && response.status === 502 && retries > 0) {
-      console.log(`Retrying request: ${request.url}, attempts left: ${retries - 1}`);
-      return new Promise(resolve => setTimeout(resolve, 500)).then(() => fetchWithRetry(request, retries - 1));
-    }
-    return response;
-  }).catch(error => {
-    if (retries > 0) {
-      console.log(`Retrying request: ${request.url}, attempts left: ${retries - 1}`);
-      return new Promise(resolve => setTimeout(resolve, 500)).then(() => fetchWithRetry(request, retries - 1));
-    } else {
-      throw error;
-    }
-  });
-}
